d52bac3094e1c3471d9b31e1d4a33f8ea9fe2061
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-29 14:46:48 +0200

    env 03 added

---
+++ environment-03/data.js
@@ -0,0 +1,32 @@
+const events = [
+    {
+        title: "Meeting with Clients",
+        description: "Discuss project requirements and deliverables",
+        date: "2023-07-29"
+    },
+    {
+        title: "Team Lunch",
+        description: "Enjoy a team lunch to celebrate project completion",
+        date: "2023-06-03"
+    },
+    {
+        title: "Conference Presentation",
+        description: "Present new product features at a tech conference",
+        date: "2023-06-22"
+    },
+    {
+        title: "Workshop: Introduction to JavaScript",
+        description: "Teach beginners the basics of JavaScript programming",
+        date: "2023-06-12"
+    },
+    {
+        title: "Webinar: UX Design Trends",
+        description: "Explore the latest trends in UX design",
+        date: "2023-08-15"
+    },
+    {
+        title: "Project Deadline",
+        description: "Submit the finalized project to the client",
+        date: "2023-06-01"
+    }
+];
